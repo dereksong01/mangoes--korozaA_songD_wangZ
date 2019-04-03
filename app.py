17e359b689544805a8c90e8268e87048b05925be
SashaKor <akoroza@stuy.edu>
2019-04-02 22:32:30 -0400

    fleshing out base html

--- app.py
+++ app.py
@@ -10,63 +10,25 @@ from util import mongo
 
 app = Flask(__name__)
 
-'''
-now handled by a func
-#SERVER_ADDR='104.248.53.196' #zane
-SERVER_ADDR='178.128.156.17' #AK
-client = MongoClient(SERVER_ADDR, 27017)
-db = client.MOZArella
-collection = db.movies
-
-
-with open('movies.json') as f:
-    lines = f.read()
-    data = json.loads(lines)
-    collection.insert_many(data)
- '''
-
-
-
 '''home route'''
 @app.route('/', methods=['POST','GET'])
 def index():
-    print("didnt go through")
+    return render_template("base.html")
+
+'''finances route (INSTRUCTION_EXPENDITURE)'''
+@app.route('/map', methods=['POST','GET'])
+def map():
     return render_template("map.html")
 
-'''imports collection to input droplet address
-@app.route("/enter",methods=['POST','GET'] )
-def enter():
-    if request.form["submit"] == "submit":
-        IP= request.form["IP"]
-        global ip
-        ip= IP
-        print(IP)
-        #SERVER_ADDR='104.248.53.196' #zane
-        client = MongoClient(IP, 27017)
-        db = client.MOZArella
-        collection = db.movies
+'''enrollment route '''
+@app.route('/map1', methods=['POST','GET'])
+def map1():
+    return render_template("map1.html")
 
-        with open('movies.json') as f:
-            lines = f.read()
-            data = json.loads(lines)
-            collection.insert_many(data)
-            global col
-            col = collection
-
-        return render_template("movies.html", IP=IP) #renders movie.html with the imported collection
-    print("stuff went down")
-    return render_template("ipset.html")
-
-Using input years and titles to return values from collection
-@app.route("/search",methods=['POST','GET'])
-def search():
-    if request.form["submit1"] == "submit1":
-        func= 1
-        year= request.form["year"]
-        result= mongo.find_year(int(year),col)
-        return render_template("detail.html", func=str(func), result=str(result))
-    return render_template("movies.html", IP= ip)
-'''
+'''achievement route '''
+@app.route('/map2', methods=['POST','GET'])
+def map2():
+    return render_template("map2.html")
 
 if __name__ == "__main__":
     app.debug = True
